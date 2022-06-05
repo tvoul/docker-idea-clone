95406b49166151f588be7458c69261c826dc5836
tvoul <t.voulgaris@hotmail.com>
2022-06-05 11:46:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     error_reporting(E_ALL);
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $database);
 
     // Check connection
     if ($conn->connect_error) {
